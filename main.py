9d5aa35b1e2836c0a9e2e52baba716d30db268e4
shnovaj30101 <shnovaj30101@gmail.com>
2022-06-24 18:26:06 +0800

    readme

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ import math
 
 class RBTreeDemo(Scene):
     def construct(self):
+        # 這邊去註解可以在背景加入網格線
         # numberplane = NumberPlane()
         # self.add(numberplane)
 
+        # start_coor 可以改變跟節點的初始位置
+        # 設定方法為 x * UP + y * RIGHT，x 和 y 可以自由設定，最後跟節點必會位於網格線上的座標 (x,y)
         rbtree_animation = RBTreeAnimation(self, start_coor = UP*2)
+
+        # 可使用 rbtree_animation.insert() 或是 rbtree_animation.delete()
+        # 來指定要插入什麼資料以及刪除什麼資料
         rbtree_animation.insert(6)
         rbtree_animation.insert(1)
         rbtree_animation.insert(5)
--- setting.py
+++ setting.py
@@ -1,8 +1,21 @@
 
-NODE_CIRCLE_RADIUS = 0.3 / 1.5
-DUMMY_CIRCLE_RADIUS = 0.35 / 1.5
-HORIZONTAL_NODE_SPACING = 0.9 / 1.5
-LAYER_HEIGHT = 0.6 / 1.5
-RUN_TIME_UNIT = 0.5 / 2 
+# 指定一個 Node 的半徑
+NODE_CIRCLE_RADIUS = 0.6 / 1.5
+
+# 指定一個 DummyNode (綠框圓圈) 的半徑
+DUMMY_CIRCLE_RADIUS = 0.65 / 1.5
+
+# 指定兩個相鄰 Node 的圓心水平距離
+HORIZONTAL_NODE_SPACING = 1.5 / 1.5
+
+# 指定兩個父子 Node 的圓心垂直距離
+LAYER_HEIGHT = 1.5 / 1.5
+
+# 指定每個單位動畫時長
+RUN_TIME_UNIT = 0.3 / 2 
+
+# 指定箭頭的最大寬度
 LINE_WIDTH = 3
+
+# 指定箭頭前端的對大 size
 TIP_LENGTH = 0.2
